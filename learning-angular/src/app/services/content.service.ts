c9743b63340437ab22882e05e5be8b233589ea5a
YonisA <55886158+YonisA@users.noreply.github.com>
2020-11-18 22:06:42 -0500

    added messages plus some funky styles :D

--- learning-angular/src/app/services/content.service.ts
+++ learning-angular/src/app/services/content.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import {Content} from '../helper-files/content-interface';
 import {ItemList} from '../helper-files/contentDb';
 import {Observable, of} from 'rxjs';
+import { MessageService } from '../message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
   listOfObservableItems(): Observable<Content[]>{
+    this.messageService.add('Content retrieved!');
     return of(ItemList);
   }
 }
